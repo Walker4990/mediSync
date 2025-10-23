24f896f0ea90ba39b973f326fd28cb0388bf8d7b
monchaeeun <127359405+monchaeeun@users.noreply.github.com>
2025-10-23 15:13:20 +0900

    예약 날짜에 따라 가능한 시간대 보여주기

---
+++ medi-sync-backend/src/main/java/com/mediSync/project/service/ReservationService.java
@@ -0,0 +1,31 @@
+package com.mediSync.project.service;
+
+import com.mediSync.project.mapper.ReservationMapper;
+import com.mediSync.project.vo.Reservation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReservationService {
+
+    private final ReservationMapper reservationMapper;
+
+    //로그인 중인 회원 정보 가져오기
+    
+    
+    // 의사 정보 가져오기
+
+    
+    //해당 날짜의 예약 정보 가져오기
+    public List<String> getReservedTimesByDate(String date){
+        return reservationMapper.getReservedTimesByDate(date);
+    }
+
+    //예약 추가하기
+    public int addReservation(Reservation reservation){
+        return reservationMapper.addReservation(reservation);
+    }
+}
--- medi-sync-frontend/src/App.js
+++ medi-sync-frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PatientList from "./pages/admin/PatientList";
 import Register from "./pages/user/Register";
 import UserHome from "./pages/user/UserHome";
+import Reservation from "./pages/user/Reservation";
 
 function App() {
    return ( <BrowserRouter>
@@ -14,6 +15,8 @@ function App() {
             {/*유저페이지*/}
             <Route path={"/"} element={<UserHome />} />/
             <Route path={"user/register"} element={<Register />} />
+             <Route path={"user/reservation"} element={<Reservation />} />
+
         </Routes>
     </BrowserRouter>
 );
