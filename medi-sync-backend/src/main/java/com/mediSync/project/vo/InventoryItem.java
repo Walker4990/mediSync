b7a4fa43255a1cf7bb48c8b8661d7bb01ef777d1
choqsang <chks1@naver.com>
2025-10-28 08:31:46 +0900

    fix : inven vo 수정

--- medi-sync-backend/src/main/java/com/mediSync/project/vo/InventoryItem.java
+++ medi-sync-backend/src/main/java/com/mediSync/project/vo/InventoryItem.java
@@ -5,9 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.ibatis.type.Alias;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Alias("inven")
 public class InventoryItem {
+    private Long itemId;
+    private String itemName, unit, supplier, location, drugCode;
+    private int quantity, minStock;
+    private BigDecimal price;
+    private LocalDate expiryDate;
+    private LocalDateTime updatedAt;
 }
